0a1921ddf5ddf1df26cd88c0a02f655b159bf556
tapankumar2001 <144725934+tapankumar2001@users.noreply.github.com>
2023-10-25 19:22:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+let day="12";
+
+switch(day){
+    case"1":
+    console.log("monday");
+    break;
+
+    case"2":
+    console.log("tuesday");
+    break;
+
+    case"3":
+    console.log("wednesdy");
+    break;
+
+    case"4":
+    console.log("thurseday");
+    break;
+
+    case"5":
+    console.log("friday");
+    break;
+
+    case"6":
+    console.log("saturday");
+    break;
+
+    case"7":
+    console.log("sunday");
+    break;
+
+    default:
+        console.log("thats not a day")
+}
+
+// alert('this is a simple alert!');
+// prompt("What is our Name?;
+
+let firstaName= prompt("Enter Your First Name!");
+let lastName= prompt("Enter Your Last Name!")
+
+alert("Welcome"+" "+firstaName+" "+lastName);
+
+console.log(firstaName+" "+lastName);
